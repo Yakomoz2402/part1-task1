c1f640a972d554a88f3f27444ad455e9e1205718
Yakamoz2402 <o776205105@gmai.com>
2020-03-09 14:08:32 +0600

    first commit

---
+++ task1.py
@@ -0,0 +1,12 @@
+'''num = int(input('введите число:'))
+if num % 2 == 0:
+    print('true')
+else:
+    print('False')'''
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
